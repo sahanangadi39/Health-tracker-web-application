61c08a55e6ebddd246c89163ae395014bdcef98a
sahan-veeresh-angadi <sahanangadi@gmail.com>
2025-01-19 10:40:08 +0530

    login page implemented

--- login.php
+++ login.php
@@ -13,7 +13,9 @@
         <a href="signup.php">Don't have an account? <b>Sign Up</b></a>
         <div class="logoformcontainer">
             <div class="logocontainer">
-                <img src="/Images/logo.png" alt="Logo" class="logo">
+                <img style="margin: 290px auto 0px 30px;" height="25px" width="fit-content" src="/Images/Group 1000005572.png" alt="logo">
+                <img style="margin: 8px auto 0px 30px;" height="14px" width="fit-content" src="/Images/Empowering Employees to Thrive.png" alt="">
+
             </div>
             <div class="formcontainer">
                 <h1>Sign In</h1>
