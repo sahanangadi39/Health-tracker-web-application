163a39cdf91bf80954c0feb855965967e731ac58
sahan-veeresh-angadi <sahanangadi@gmail.com>
2025-01-19 11:14:47 +0530

    Home page  and navbar development

---
+++ navbar.php
@@ -0,0 +1,19 @@
+
+<header>
+    <div class="logo"><img style="filter: invert(100%);" src="/Images/Group 1000005572.png" alt="logo"></div>
+    <nav>
+        <ul>
+            <a href="#">Health tracker</a>
+            <a href="#">AI assistant</a>
+            <a href="#">Devices</a>
+            <a href="#">Blog</a>
+            <a href="#">About Us</a>
+        </ul>
+    </nav>
+    <div class="accountsection">
+        <img src="/Images//help.png" alt="Help">
+        <img src="/Images/notification.png" alt="Notifications">
+        <img src="/Images/account.png" alt="Account">
+    </div>
+
+</header>
