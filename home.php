da9f3c5a4d61c3b7cba0fc83d6c58f91ac9b12ac
sahan-veeresh-angadi <sahanangadi@gmail.com>
2025-01-20 21:17:49 +0530

    updated font to open sans in navbar

--- home.php
+++ home.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="/Css/navigation.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
     <!-- navbar  -->
     <?php
     require 'navbar.php';
     ?>
-<div class="dash">
-    
-</div>
+    <div class="dash">
+
+    </div>
 </body>
+
 </html>
