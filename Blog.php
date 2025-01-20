4130e3774c6605df6107e6c697563fdf5f93c05c
sahan-veeresh-angadi <sahanangadi@gmail.com>
2025-01-20 23:50:13 +0530

    implemented blog page

--- Blog.php
+++ Blog.php
@@ -5,11 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blogs</title>
+
+    <link rel="stylesheet" href="/Css/Blog.css">
     <link rel="stylesheet" href="/Css/navigation.css">
+
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 </head>
 
@@ -17,6 +24,34 @@
     <?php
     require 'navbar.php';
     ?>
+    <div id="preventationcontainer">
+        <div class="blogleftimage">
+            <img src="/Images/Blog/box-nuts-spices-wood-table.png" alt="nuts-spices-wood-table image">
+        </div>
+        <div class="blogrightcontent">
+            <h2>Prevention is Better Than Cure</h2>
+
+            The saying "prevention is better than cure" emphasises the importance of proactive measures to avoid problems
+            rather than dealing with their consequences. Maintaining a healthy lifestyle with proper nutrition and exercise
+            reduces the risk of diseases, while regular safety checks prevents accidents. By addressing potential issues before
+            they escalate, we foster a more sustainable and efficient approach to problem-solving, benefiting individuals and
+            society as a whole.
+        </div>
+    </div>
+
+
+    <div id="healthblogs">
+        <img src="/Images/Blog/nutrition.png" alt="Nutrition">
+        <img src="/Images/Blog/hyderation.png" alt="Hyderation">
+        <img src="/Images/Blog/goodposture.png" alt="Good posture">
+        <img src="/Images/Blog/exercise.png" alt="Exercise">
+        <img src="/Images/Blog/hearthealth.png" alt="Heart Health">
+        <img src="/Images/Blog/stressmanagement.png" alt="Stress management">
+    </div>
+
+<?php
+    require 'footer.php';
+?>
 </body>
 
 
---
+++ footer.php
@@ -0,0 +1,52 @@
+<head>
+    <link rel="stylesheet" href="/Css/footer.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<footer>
+    
+    <div id="footerhyperlinks">
+        <div>
+            <a href="">Home</a>
+            <a href="">Health tracking</a>
+            <a href="">Health analysis</a>
+            <a href="">Mental health support</a>
+        </div>
+
+        <div>
+            <a href="">Ai assistant</a>
+            <a href="">Daily reports</a>
+            <a href="">Notifications</a>
+            <a href="">Daily Tasks</a>
+        </div>
+
+        <div>
+            <a href="">Profile</a>
+            <a href="">Blog</a>
+            <a href="">About Us</a>
+            <a href="">Contact</a>
+        </div>
+
+        <div>
+            <p>Contact Us</p>
+            <p>Support@healthtracker.ai</p>
+            <p>&#128222; +1-800-123-4567</p>
+            <p>&copy; <?php echo date("Y") ?> HealthTracker.ai. All Rights Reserved.</p>
+            <p>Commited to your health, every step of the way</p>
+        </div>
+    </div>
+
+    <div id="socialmedia">
+        <img src="/Images/footer/whatsapp.png" alt="Whatsapp">
+        <img src="/Images/footer/facebook.png" alt="Facebook">
+        <img src="/Images/footer/instagram.png" alt="Instagram">
+        <img src="/Images/footer/youtube.png" alt="Youtube">
+        <img src="/Images/footer/linkedin.png" alt="Linkedin">
+        <img src="/Images/footer/pintrest.png" alt="Pintrest">
+        <img src="/Images/footer/telegram.png" alt="Telegram">
+    </div>
+
+</footer>
