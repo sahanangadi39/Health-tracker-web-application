117036c81b6bf21da405145c96916bfec10c1cc6
sahan-veeresh-angadi <sahanangadi@gmail.com>
2025-01-20 18:00:24 +0530

    fixed the login page apperance

--- login.php
+++ login.php
@@ -10,6 +10,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Health is Wealth</title>
     <link rel="stylesheet" href="/Css/login.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -17,8 +21,8 @@
         <a href="signup.php">Don't have an account? <b>Sign Up</b></a>
         <div class="logoformcontainer">
             <div class="logocontainer">
-                <img style="margin: 290px auto 0px 30px;" height="25px" width="fit-content" src="/Images/Group 1000005572.png" alt="logo">
-                <img style="margin: 8px auto 0px 30px;" height="14px" width="fit-content" src="/Images/Empowering Employees to Thrive.png" alt="">
+                <img style="margin: 240px auto 0px 20px;" height="20px" width="fit-content" src="/Images/Group 1000005572.png" alt="logo">
+                <img style="margin: 8px auto 0px 20px;" height="14px" width="fit-content" src="/Images/Empowering Employees to Thrive.png" alt="">
 
             </div>
             <div class="formcontainer">
