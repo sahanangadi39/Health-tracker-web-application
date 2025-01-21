565cc6a7ad494dc28b03a263521072143fe39a73
Luvkuxh <jainuniluvkush@gmail.com>
2025-01-21 13:45:09 +0530

    added homepage anad css of  homepage

--- home.php
+++ home.php
@@ -14,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="/Css/navigation.css">
+    <link rel="stylesheet" href="/Css/dash.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -26,7 +27,17 @@
     <?php
     require 'navbar.php';
     ?>
+
     <div class="dash">
+        <div class="skill">
+            <div class="outer">
+                <div class="inner">
+                    
+                </div>
+            </div>
+        </div>
+
+
         
     </div>
     <?php require 'footer.php'; ?>
