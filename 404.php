8baecdb03b059b9c81d6ef061797d0289e1ae949
= <sahan@sahan.in>
2025-02-02 06:33:21 +0000

    404 backend

---
+++ 404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page doesn't exist</title>
+    <link rel="stylesheet" href="/Css/navigation.css">
+    <link rel="stylesheet" href="/Css/footer.css">
+</head>
+<body>
+    <?php
+        require 'navbar.php';
+    ?>
+         <h1><center>Error 404 Page not found</center></h1> <!-- Comment this once done with you frontent code  -->
+     <!-- Luvkush write your frontend code here  -->
+
+    <?php
+        require 'footer.php'
+    ?>
+</body>
+</html>
