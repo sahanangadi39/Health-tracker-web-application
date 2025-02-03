6162930b7838e41706f709eefe52a41ba4cbc99c
Luvkuxh <jainuniluvkush@gmail.com>
2025-02-04 04:32:53 +0530

    devices page done

--- devices.php
+++ devices.php
@@ -4,8 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Devices</title>
+    <title>Exercise</title>
+    <link rel="stylesheet" href="/Css/devices.css">
+    <link rel="stylesheet" href="/Css/Blogpage.css">
     <link rel="stylesheet" href="/Css/navigation.css">
+    <link rel="stylesheet" href="/Css/footer.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -19,6 +22,54 @@
     <?php
     require 'navbar.php';
     ?>
-</body>
 
-</html>
+<section class="device-container">
+    <div class="paired-devices-card">
+        <h3>Paired Devices</h3>
+        <ul>
+            <li>
+                Apple Watch
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider"></span>
+                </label>
+            </li>
+            <li>
+                iPhone 16 Pro
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider"></span>
+                </label>
+            </li>
+            <li>
+                MacBook Pro
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider"></span>
+                </label>
+            </li>
+            <li>
+                Google Pixel
+                <label class="switch">
+                    <input type="checkbox">
+                    <span class="slider"></span>
+                </label>
+            </li>
+        </ul>
+    </div>
+
+    <div class="scanning-area">
+        <div class="scanning-animation">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <button class="scan-btn">Scanning for Devices</button>
+        </div>
+    </div>
+</section>
+
+<?php
+    require 'footer.php';
+    ?>
+
+</body>
