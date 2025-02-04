0419f716db8d917fe3ab3546f7b4d61965fe9209
Luvkuxh <jainuniluvkush@gmail.com>
2025-02-04 06:04:52 +0530

    landing page done

---
+++ landing_page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FitForce - Employee Wellness</title>
+    <link rel="stylesheet" href="/Css/landing_page.css">
+    <link rel="stylesheet" href="/Css/devices.css">
+    <link rel="stylesheet" href="/Css/Blogpage.css">
+    <link rel="stylesheet" href="/Css/navigation.css">
+    <link rel="stylesheet" href="/Css/footer.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="/Images/favicon.svg" type="image/x-icon">
+
+
+</head>
+
+<body>
+    <?php
+    require 'navbar.php';
+    ?>
+
+<div class="container">
+        <div class="overlay"></div>
+        <div class="content-card">
+            <img src="Images/logo1.png" alt="FitForce Logo" class="logo-image">
+            <h1>Empowering Employees to Thrive</h1>
+            <p>Join our journey to a healthy working life.<br>We got you covered.</p>
+            <button class="signup-btn">Sign Up</button>
+        </div>
+    </div>
+
+    <?php
+        require 'footer.php';
+        ?>
+
+</body>
+</html>
